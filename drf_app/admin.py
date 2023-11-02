87d23e2146208f6809d42b239e7c4c9e694e9817
Gizar <gizarir@gmail.com>
2023-11-02 14:20:46 +0200

    Added tables to admin board

--- drf_app/admin.py
+++ drf_app/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 
 from .models import *
 
- # admin.site.register(Vocabulary)
+admin.site.register(Vocabulary)
+admin.site.register(Lemma)
+admin.site.register(VocabularyLemma)
+admin.site.register(Lang)
 
