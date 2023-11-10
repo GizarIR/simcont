ce9436efb0ebf7aa6e4948cf656e78f95eaeaba5
Gizar <gizarir@gmail.com>
2023-11-10 10:17:33 +0200

    Added Swagger by drf-yasg, and path for templates

--- simcont/settings.py
+++ simcont/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'debug_toolbar',
     'rest_framework',
+    'drf_yasg',
     'users.apps.UsersConfig',
     'drf_app.apps.Drf_appConfig',
 ]
@@ -73,7 +74,7 @@ ROOT_URLCONF = 'simcont.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, 'drf_app/templates/drf_app/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- simcont/urls.py
+++ simcont/urls.py
@@ -21,15 +21,38 @@ from . import settings
 from drf_app.views import *
 from rest_framework import routers
 
+# For Swagger
+from django.urls import re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 # More information https://proproprogs.ru/django/drf-simplerouter-i-defaultrouter
 router = routers.DefaultRouter()
 router.register(r'vocabulary', VocabularyViewSet)
 router.register(r'lemma', LemmaViewSet)
 
+# For Swagger
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path("__debug__/", include("debug_toolbar.urls")),
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/v1/', include(router.urls)),   # http://127.0.0.1:8000/api/v1/.../ CRUD
 ]
 
