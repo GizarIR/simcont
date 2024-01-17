be2903a1393c142e73888c24ebeed2fcdf4bb89a
Gizar <gizarir@gmail.com>
2024-01-17 15:09:41 +0200

    Updated 2  TODO list

--- drf_app/tests.py
+++ drf_app/tests.py
@@ -1,3 +1,5 @@
 from django.test import TestCase
 
 # TODO Create your tests here.
+#  https://www.django-rest-framework.org/api-guide/testing/#testing
+#  https://chat.openai.com/c/709f23ba-47c8-4d27-aa58-2c1d1655003c
