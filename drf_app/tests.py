8cdec0c74e669630e3128d97aaf3560b8acc55e1
Gizar <gizarir@gmail.com>
2024-02-12 13:23:43 +0200

    Added connection in th middle BaseUserCase and BaseViewTestCase

--- drf_app/tests.py
+++ drf_app/tests.py
@@ -203,30 +203,13 @@ class LangUtilsTestCase(unittest.TestCase):
 #     @classmethod
 #     def setUpClass(cls):
 
-class BaseTestCase(BaseUserCase):
-    pass
-
-
-class VocabularyTests(APITestCase):
+class BaseViewTestCase(BaseUserCase):
     @classmethod
-    def setUpTestData(cls):
-        user_data = {
-            "email": "test@example.com",
-            "password": "testpassword",
-        }
-
+    def setUpClass(cls):
+        super().setUpClass()
         # Create common data
         cls.lang_from = Lang.objects.create(name='English', short_name='en')
         cls.lang_to = Lang.objects.create(name='Russian', short_name='ru')
-
-        post_save.disconnect(send_activation_email, sender=CustomUser)
-        user = CustomUser.objects.create_user(**user_data)
-        user.is_active = True
-        user.activation_code = None
-        user.save()
-        cls.user = user
-        post_save.connect(send_activation_email, sender=CustomUser)
-
         cls.vocabulary_data = {
             'title': 'Test Vocabulary',
             'description': 'Test Description',
@@ -236,31 +219,39 @@ class VocabularyTests(APITestCase):
             'author': str(cls.user.id),
             'learners_id': [str(cls.user.id)],
         }
-
-        access_token = AccessToken.for_user(cls.user)
-        cls.access_token = str(access_token)
-        cls.client = APIClient()
-        cls.client.credentials(HTTP_AUTHORIZATION=f'Bearer {cls.access_token}')
-
         # Create instance of Vocabulary for tests
         post_save.disconnect(order_lemmas_create, sender=Vocabulary)  # without fill order_lemmas field
 
         url = reverse('vocabulary-list')
-        response = cls.client.post(url, cls.vocabulary_data, format='json')
+        response = cls.authenticated_client.post(url, cls.vocabulary_data, format='json')
         cls.created_vocabulary = Vocabulary.objects.get(pk=response.json()['id'])
 
         post_save.connect(order_lemmas_create, sender=Vocabulary)
 
+        create_order_lemmas_async(Vocabulary.objects.first().id)
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        # Clean up any data as needed
+        cls.lang_from.delete()
+        cls.lang_to.delete()
+        cls.created_vocabulary.delete()
+
+class VocabularyTests(BaseViewTestCase):
+    @classmethod
+    def setUpTestData(cls):
+        logger.info(f"***** Create test data for {cls.__name__} *****")
+
     def setUp(self):
         # Check auth
-        profile_response = VocabularyTests.client.get('/users/profile/', format='json')
+        profile_response = self.authenticated_client.get('/users/profile/', format='json')
         if profile_response.status_code != status.HTTP_200_OK:
             logger.info(f"Login for VocabularyTests failed . Test response content: {profile_response.content}")
 
     def test_create_vocabulary(self):
         logger.info(f"test_create_vocabulary")
         # order_lemmas_create(sender=Vocabulary, instance=self.created_vocabulary, created=True)
-        create_order_lemmas_async(self.created_vocabulary.id)
         self.assertEqual(Vocabulary.objects.count(), 1)
         self.assertIsNotNone(Vocabulary.objects.get(pk=self.created_vocabulary.id).order_lemmas)
         self.assertEqual(Lemma.objects.count(), 3)
@@ -269,21 +260,21 @@ class VocabularyTests(APITestCase):
     def test_retrieve_vocabulary(self):
         logger.info(f"test_retrieve_vocabulary")
         url = reverse('vocabulary-detail', args=[str(self.created_vocabulary.id)])
-        response = VocabularyTests.client.get(url)
+        response = self.authenticated_client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['title'], 'Test Vocabulary')
 
     def test_list_vocabulary(self):
         logger.info(f"test_list_vocabulary")
         url = reverse('vocabulary-list')
-        response = VocabularyTests.client.get(url)
+        response = self.authenticated_client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data["results"]), 1)
 
     def test_list_vocabulary_languages(self):
         logger.info(f"test_list_vocabulary_languages")
         url = reverse('vocabulary-languages')
-        response = VocabularyTests.client.get(url)
+        response = self.authenticated_client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 2)
         self.assertEqual(response.data[0]['name'], 'English')
@@ -291,7 +282,7 @@ class VocabularyTests(APITestCase):
     def test_get_vocabulary_language(self):
         logger.info(f"test_get_vocabulary_language")
         url = reverse('vocabulary-language', args=[str(self.created_vocabulary.lang_from.id)])
-        response = VocabularyTests.client.get(url)
+        response = self.authenticated_client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['name'], 'English')
 
@@ -301,17 +292,21 @@ class VocabularyTests(APITestCase):
             'is_active': False
         }
         url = reverse('vocabulary-detail', args=[str(self.created_vocabulary.id)])
-        response = VocabularyTests.client.patch(url, changing_data, format='json')
+        response = self.authenticated_client.patch(url, changing_data, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["is_active"], False)
 
         # Return values is_active for other tests
-        response = VocabularyTests.client.patch(url, {'is_active': True}, format='json')
+        response = self.authenticated_client.patch(url, {'is_active': True}, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 
-class LemmaTests(BaseTestCase):
+class LemmaTests(BaseViewTestCase):
+    @classmethod
+    def setUpTestData(cls):
+        logger.info(f"***** Create test data for {cls.__name__} *****")
+
     # @classmethod
     # def setUpTestData(cls):
     #     cls.lemma_data = {
@@ -325,7 +320,7 @@ class LemmaTests(BaseTestCase):
 
     def test_get_lemma(self):
         logger.info(f"test_get_lemma")
-        print(Vocabulary.objects.all())
+        print(Vocabulary.objects.all(), self.created_vocabulary.id, Vocabulary.objects.first().order_lemmas)
 
 
 if __name__ == '__main__':
