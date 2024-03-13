d8a2e8321f9d29441d539bba01dbd4d0cb46ca63
Gizar <gizarir@gmail.com>
2024-03-13 14:12:20 +0200

    Added test_refresh_token, updated created CustomTokenRefreshView

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 
-from .views import EmailTokenObtainPairView, RegisterView, ChangePasswordView, UserProfileView, UserActivationView
+from .views import EmailTokenObtainPairView, RegisterView, ChangePasswordView, UserProfileView, UserActivationView, \
+    CustomTokenRefreshView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='user_register'),
     path('token/obtain/', EmailTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
     path('change-password/', ChangePasswordView.as_view(), name='change_password'),
     path('profile/', UserProfileView.as_view(), name='user_profile'),
     path('activate/', UserActivationView.as_view(), name='user_activate'),
